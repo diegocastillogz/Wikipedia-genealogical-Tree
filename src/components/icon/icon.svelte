0839cfa4cc18ed21e2ed01a4df1d2eb26ebce006
diegocastillogz <diegorlando5@gmail.com>
2023-01-05 21:41:56 -0500

    adding functions to the character-box component to scroll to parents, open wikipedia and opens the tree of the selected character

---
+++ src/components/icon/icon.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import GenderMaleIcon from '../../assets/icons/gender-male.svelte';
+	import GenderFemaleIcon from '../../assets/icons/gender-female.svelte';
+	import GenderWikipediaIcon from '../../assets/icons/wikipedia.svelte';
+	import TreeIcon from '../../assets/icons/tree.svelte';
+
+	export let iconName: 'genderMale' | 'genderFemale' | 'wikipedia' | 'tree';
+
+	const Icons = {
+		genderMale: GenderMaleIcon,
+		genderFemale: GenderFemaleIcon,
+		wikipedia: GenderWikipediaIcon,
+		tree: TreeIcon
+	};
+</script>
+
+<div class="icon">
+	<svelte:component this={Icons[iconName]} />
+</div>
+
+<style>
+	.icon,
+	.icon :global(svg) {
+		margin: 0 auto;
+		width: 20px;
+		height: 20px;
+	}
+</style>
